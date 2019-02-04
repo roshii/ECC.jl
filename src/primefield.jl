0d549066683d2ccdeb9d9ffbf3cd4ff61b5b634e
Simon <simon@localhost.localdomain>
2019-02-04 17:15:27 +0100

    Switch to MIT license

--- src/primefield.jl
+++ src/primefield.jl
@@ -1,22 +1,3 @@
-"""
-    Copyright (C) 2018 Simon Castano
-
-    This file is part of ECC.jl
-
-    ECC.jl is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-
-    ECC.jl is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with ECC.jl.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 abstract type PrimeField <: Number end
 
 infield(x::Number,y::Number) = x >= 0 && x < y
